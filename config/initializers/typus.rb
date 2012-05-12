c7a86a642f2981c7348bb2bcda40eb0a3bf9dd49
baban <babanba.n@gmail.com>
2012-05-12 10:31:02 +0900

    管理画面日修正

--- config/initializers/typus.rb
+++ config/initializers/typus.rb
@@ -1,8 +1,10 @@
+# encoding: utf-8
+#
 Typus.setup do |config|
 
   # Application name.
-  # config.admin_title = "Typus"
-  # config.admin_sub_title = ""
+  config.admin_title = "RAS_VN:管理画面"
+  config.admin_sub_title = "shops innnovation"
 
   # When mailer_sender is set, password recover is enabled. This email
   # address will be used in Admin::Mailer.
@@ -15,7 +17,7 @@ Typus.setup do |config|
   # This is a required Rails configuration. In production it must be the actual
   # actual host of your application.
   #
-  # config.mailer_sender = "admin@example.com"
+  config.mailer_sender = "babanba.n@gmail.com"
 
   # Define paperclip attachment styles.
   # config.file_preview = :medium
