faf37cc5092f92414951d0f88060c7127784fe1f
baban <babanba.n@gmail.com>
2012-05-19 01:36:18 +0900

    レシピステップ編集画面

--- app/controllers/recipe_steps_controller.rb
+++ app/controllers/recipe_steps_controller.rb
@@ -2,5 +2,10 @@
 
 class RecipeStepsController < ApplicationController
   def update
+    @recipe = Recipe.find(params[:id])
+    steps = params[:recipe_steps].values.select{ |v| v.blank?.! }.map{ |v| RecipeStep.new(context: v) }
+    @recipe.recipe_steps= steps
+    @recipe.save!
+    @recipe.steps
   end
 end
