9858fbe64cde77855250e225115af258a8aa95e6
baban <babanba.n@gmail.com>
2012-05-25 21:59:57 +0900

    食材名で探せるように修正

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,11 @@
+# encoding: utf-8
+
 class RecipeFoodsController < ApplicationController
   def index
+    if params[:recipe_food_genre_id]
+      @food_genres = RecipeFoodGgenre.where( " id = ? ", params[:recipe_food_genre_id] )
+    else
+      @food_genres = RecipeFoodGenre.all
+    end
   end
 end
