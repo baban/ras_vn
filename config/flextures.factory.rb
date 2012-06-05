cd580ad5910093faab94f668ecafd0c2098acc83
baban <babanba.n@gmail.com>
2012-06-05 09:18:51 +0900

    レシピ食材追加

--- config/flextures.factory.rb
+++ config/flextures.factory.rb
@@ -5,11 +5,11 @@ Flextures::Factory.define :users do |f|
 end
 
 Flextures::Factory.define :restaurant_profiles do |f|
-  filename = Rails.root.to_path + "/app/assets/images/tp_m.jpg"
-  f.top_photo = IO.read("#{filename}")
+  #filename = Rails.root.to_path + "/app/assets/images/tp_m.jpg"
+  #f.top_photo = IO.read("#{filename}")
 end
 
 Flextures::Factory.define :recipes do |f|
-  filename = Rails.root.to_path + "/app/assets/images/recipe_img.jpg"
-  f.image = IO.read("#{filename}")
+  #filename = Rails.root.to_path + "/app/assets/images/recipe_img.jpg"
+  #f.image = IO.read("#{filename}")
 end
