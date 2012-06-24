c5dff63d77c123fdd80f6c1555b34b2318ecd45d
baban <babanba.n@gmail.com>
2012-06-24 12:24:31 +0900

    レシピ作成

--- app/uploaders/recipe_steps_uploader.rb
+++ app/uploaders/recipe_steps_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class ResipeStepsUploader < CarrierWave::Uploader::Base
+class RecipeStepsUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -36,9 +36,9 @@ class ResipeStepsUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :scale => [150, 150]
-  end
+  #version :thumb do
+  #  process :scale => [150, 150]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
