61263c6da3e632c963eaee678be581ddf4f61238
baban <babanba.n@gmail.com>
2012-07-06 23:53:55 +0900

    日記作成

--- app/controllers/diary_controller.rb
+++ app/controllers/diary_controller.rb
@@ -4,21 +4,38 @@ class DiaryController < ApplicationController
   def index
     id = params[:user_id]
     @user = User.find_by_id(id)
-    @articles = @user.diary
+    @diary = @user.diary
   end
 
   def show
+    @diary = Diary.find(params[:id])
+  end
+
+  def new
   end
 
   def create
+    diary = Diary.new
+    diary.attributes= params[:diary]
+    diary.save
+
+    redirect_to :index, notice:"記事を作成しました"
   end
 
   def edit
   end
 
   def update
+    diary = Diary.find(params[:id])
+    diary.attributes= params[:diary]
+    diary.save
+
+    redirect_to :index, notice:"記事を更新しました"
   end
 
   def destroy
+    Diary.find(params[:id]).destroy
+    
+    redirect_to :index, notice:"削除が完了しました"
   end
 end
