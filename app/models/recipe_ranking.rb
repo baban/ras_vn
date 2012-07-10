60fb731dbbc8c2666cd2fe293bfc529b7dc6974e
baban <babanba.n@gmail.com>
2012-07-10 09:54:41 +0900

    人気レシピのランキング作成

--- app/models/recipe_ranking.rb
+++ app/models/recipe_ranking.rb
@@ -9,4 +9,11 @@ class RecipeRanking < ActiveRecord::Base
     ids = page(1).per(2).all.map(&:recipe_id)
     Recipe.where( "id in (?) ", ids )
   end
+
+  # create ranking
+  def self.aggrigate
+    # ranking sorted by only page view_count
+    ranking = Recipe.order("view_count desc").limit(10)
+    ranking.map{ |recipe| self.create( recipe_id: recipe.id ) }
+  end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -17,4 +17,8 @@
 #   runner "AnotherModel.prune_old_records"
 # end
 
+every 1.days do
+  runner "RecipeRanking.aggrigation"
+end
+
 # Learn more: http://github.com/javan/whenever
