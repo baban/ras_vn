371bbcd947dde34b370b67f24fb67b97ceeb2383
baban <babanba.n@gmail.com>
2012-07-16 12:52:06 +0900

    users読み込み機能

--- config/flextures.factory.rb
+++ config/flextures.factory.rb
@@ -14,3 +14,22 @@ Flextures::Factory.define :recipes do |f|
   f
 end
 
+Flextures::Factory.define :admin_users do |f|
+  p :preferences
+  p f.preferences
+  f.preferences = Base64.decode64(f.preferences)
+  p f.preferences
+  f
+end
+
+Flextures::DumpFilter.define :admin_users, {
+  preferences:->(v){ Base64.encode64(v.to_yaml) }
+}
+
+
+Flextures::Factory.define :users do |f|
+  tmp_pass = f.encrypted_password
+  f.password = tmp_pass
+  f.encrypted_password = tmp_pass
+  f
+end
