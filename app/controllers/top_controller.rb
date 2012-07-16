ee8a7d7b115333c298ee69fd783436efe3bd4c54
baban <babanba.n@gmail.com>
2012-07-16 19:10:43 +0900

    topページのspecすり合わせ

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,6 +3,7 @@
 class TopController < ApplicationController
   before_filter :sidebar_ranking_filter
   def index
+    logger.info session.inspect
     top_content = ToppageContent.first
     @recomment_food_genre_recipe = Recipe.find_by_id(top_content.recommend_recipe_genre_id)
     @recommend_recipe = Recipe.find_by_id(top_content.recommend_recipe_id)
--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -5,6 +5,9 @@ require 'spec_helper'
 describe TopController do
 
   describe "GET 'index'" do
+    before do
+      session["session_id"]="21b06649224817f12cad479a53eacb36"
+    end
     it "returns http success" do
       get 'index'
       response.should be_success
