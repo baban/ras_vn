96288e70af788fceb5e792555dc14070956f1448
baban <babanba.n@gmail.com>
2012-07-21 18:54:41 +0900

    マイページ編集

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -17,6 +17,6 @@ module ProfileHelper
   def visibility_radios( visibility, attr_name )
     btn1 = radio_button( "user_profile_visibility", attr_name, 1, checked: (visibility.send(attr_name) ?   "checked" : nil) )
     btn2 = radio_button( "user_profile_visibility", attr_name, 0, checked: (visibility.send(attr_name).! ? "checked" : nil) )
-    "公開#{btn1}非公開#{btn2}".html_safe
+    "#{t('status.public')}#{btn1}#{t('status.closed')}#{btn2}".html_safe
   end
 end
