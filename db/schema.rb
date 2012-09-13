867dbf618308c56a8bdf057698134950e02a87d0
hoang <hoang@okwave.co.jp>
2012-09-13 14:49:08 +0900

    schema更新

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(:version => 20120908081140) do
     t.text     "content",                                        :null => false
     t.integer  "category_id", :default => 0,                     :null => false
     t.string   "image"
-    t.datetime "publiced_at", :default => '2012-09-10 00:00:00', :null => false
+    t.datetime "publiced_at", :default => '2012-09-13 00:00:00', :null => false
     t.datetime "deleted_at"
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
@@ -143,7 +143,7 @@ ActiveRecord::Schema.define(:version => 20120908081140) do
   create_table "recipe_food_genre_rankings", :force => true do |t|
     t.integer  "recipe_food_genre_id",                           :null => false
     t.integer  "point",                :default => 0,            :null => false
-    t.date     "ranked_at",            :default => '2012-09-10', :null => false
+    t.date     "ranked_at",            :default => '2012-09-13', :null => false
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
   end
