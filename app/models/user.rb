a0e778518c13e7480beef2015ce27808a5233a8c
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-09-13 16:13:11 +0900

    データベース変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 class User < ActiveRecord::Base
+  establish_connection "ras_vn_users" if [:staging,:production].include?(Rails.env.to_sym)
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
