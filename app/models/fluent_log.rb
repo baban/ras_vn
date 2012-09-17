47e90584e806d8dd25b88cdf3e38a72089fbe246
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-09-17 14:01:42 +0900

    ログ集計メソッド作成

--- app/models/fluent_log.rb
+++ app/models/fluent_log.rb
@@ -1,3 +1,7 @@
+# encoding: utf-8
+
 class FluentLog
   include Mongoid::Document
+  # db.fluent_logs.group({ key: { path: true }, cond:{}, reduce: function(a,b){ b.sum++; }, initial:{ sum: 0 } })
+  
 end
