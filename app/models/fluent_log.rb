d9c852f195c90c7df04fff5dc63ad18c18cf5227
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-09-17 19:12:32 +0900

    ログ保管方法修正

--- app/models/fluent_log.rb
+++ app/models/fluent_log.rb
@@ -2,6 +2,16 @@
 
 class FluentLog
   include Mongoid::Document
+
+  field :host, type: String
+  field :path, type: String
+  field :user, type: String
+  field :method, type: String
+  field :code, type: Integer
+  field :size, type: Integer
+  field :referer, type: String
+  field :agent, type: String
+  field :time, type: DateTime
+
   # db.fluent_logs.group({ key: { path: true }, cond:{}, reduce: function(a,b){ b.sum++; }, initial:{ sum: 0 } })
-  
 end
