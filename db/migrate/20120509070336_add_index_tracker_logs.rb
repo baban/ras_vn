540e0a08a439df42d76b14651433f4a3acf59713
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-09-21 22:49:55 +0900

    テーブル微調整

--- db/migrate/20120509070336_add_index_tracker_logs.rb
+++ db/migrate/20120509070336_add_index_tracker_logs.rb
@@ -3,9 +3,11 @@
 class AddIndexTrackerLogs < ActiveRecord::Migration
   def up
     add_index :tracker_logs, :session_id, unique: true
+    add_index :tracker_logs, :user_id      
   end
 
   def down
     remove_index :tracker_logs, :session_id
+    remove_index :tracker_logs, :user_id      
   end
 end
--- db/migrate/20120512102036_create_affiriate_logs.rb
+++ db/migrate/20120512102036_create_affiriate_logs.rb
@@ -3,7 +3,7 @@
 class CreateAffiriateLogs < ActiveRecord::Migration
   def change
     create_table :affiriate_logs do |t|
-      t.string   :session_id,     null: false
+      t.string   :session_id,     null: false, unique: true
       t.string   :affiriate_code, null: false
       t.integer  :affiriate_type, null: false
       t.integer  :user_id,        null: true
