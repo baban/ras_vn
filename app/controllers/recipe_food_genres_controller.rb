de80605dd766c33c90ad8c039bea0404d9262afa
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-10-07 19:07:15 +0900

    大ジャンルページ

--- app/controllers/recipe_food_genres_controller.rb
+++ app/controllers/recipe_food_genres_controller.rb
@@ -1,4 +1,11 @@
 # encoding: utf-8
 
 class RecipeFoodGenresController < ApplicationController
+  def index
+    if params[:recipe_food_genre_id]
+      @food_genres = RecipeFoodGenre.where( " id = ? ", params[:recipe_food_genre_id] ).includes(:recipe_foods)
+    else
+      @food_genres = RecipeFoodGenre.includes(:recipe_foods)
+    end
+  end
 end
