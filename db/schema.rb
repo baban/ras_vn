38789fddda2caee4389c800eb93ea1c22b5fa5bf
truongdinhhoang <truongdinhhoang80@hoang.lan>
2012-10-08 23:59:33 +0900

    recipeマスターデータ作成

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(:version => 20121007142802) do
     t.text     "content",                                        :null => false
     t.integer  "category_id", :default => 0,                     :null => false
     t.string   "image"
-    t.datetime "publiced_at", :default => '2012-09-21 00:00:00', :null => false
+    t.datetime "publiced_at", :default => '2012-09-19 00:00:00', :null => false
     t.datetime "deleted_at"
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
@@ -168,7 +168,7 @@ ActiveRecord::Schema.define(:version => 20121007142802) do
   create_table "recipe_food_genre_rankings", :force => true do |t|
     t.integer  "recipe_food_genre_id",                           :null => false
     t.integer  "point",                :default => 0,            :null => false
-    t.date     "ranked_at",            :default => '2012-09-21', :null => false
+    t.date     "ranked_at",            :default => '2012-09-19', :null => false
     t.datetime "created_at",                                     :null => false
     t.datetime "updated_at",                                     :null => false
   end
@@ -302,8 +302,7 @@ ActiveRecord::Schema.define(:version => 20121007142802) do
   add_index "toppage_contents", ["recommend_recipe_id"], :name => "index_toppage_contents_on_recommend_recipe_id"
 
   create_table "tpl_sets", :force => true do |t|
-    t.string   "name",       :null => false
-    t.text     "content",    :null => false
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
