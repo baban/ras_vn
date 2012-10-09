f240a2d4d0b0e2221f1fb16fec2432dab8cae1e4
babanba.n <babanba.n@gmail.com>
2012-10-10 01:35:48 +0900

    deploy設定調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # users table and user_profiles table are saved data to another database
-  establish_connection "ras_vn_users" if [:staging,:production].include?(Rails.env.to_sym)
-
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
