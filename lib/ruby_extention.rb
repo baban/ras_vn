b01fb415a92669401c1517b6a9658ef0cc8a1fd7
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-10-11 09:47:27 +0900

    開発向けユーティリティ追加

--- lib/ruby_extention.rb
+++ lib/ruby_extention.rb
@@ -8,7 +8,21 @@ end
 
 class Object
   def do(*args,&b)
-    yield self, *args
+    yield(self, *args)
+  end
+
+  def method_hooking( name, &b )
+    (method(name).to_proc * ->(*_){ b.call(*_) }).do { |p| define_method(name) &p }
+  end
+
+  def tap_info(*_)
+    Rails.logger.info yield(*_) if block_given?
+    self
+  end
+
+  def tap_error(*_)
+    Rails.logger.error yield(*_) if block_given?
+    self
   end
 end
 
