b0e8ce2f66bf60d682cdb353608df6a419f24568
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-10-12 00:27:58 +0900

    ログ集計メソッド

--- app/models/fluent_log.rb
+++ app/models/fluent_log.rb
@@ -1,17 +1,61 @@
 # encoding: utf-8
 
 class FluentLog
-  include Mongoid::Document
+  include Mongoid::Document  
 
-  field :host, type: String
-  field :path, type: String
-  field :user, type: String
-  field :method, type: String
-  field :code, type: Integer
-  field :size, type: Integer
+  field :code,    type: Integer
+  field :host,    type: String
+  field :path,    type: String
+  field :size,    type: Integer
   field :referer, type: String
-  field :agent, type: String
-  field :time, type: DateTime
+  field :agent,   type: String
+  field :time,    type: DateTime
 
-  # db.fluent_logs.group({ key: { path: true }, cond:{}, reduce: function(a,b){ b.sum++; }, initial:{ sum: 0 } })
+  def self.aggrigation
+    # self.page_flow_aggrigation
+    page_map = self.page_count_aggrigation
+    move_log = MoveLog.aggrigate_log
+    move_log = MoveLog.translate_aggrigated_log( move_log, page_map )
+
+    ret = {
+      nodes: page_map.map{ |k,v| { label: k, amount: v } }.sort{ |a,b| b[:amount] <=> b[:amount] },
+      links: move_log
+    }
+    ret
+  end
+
+  def self.page_count_aggrigation
+    map = %Q[ function () { emit(this.path, {}); } ]
+    reduce = %Q[ function (key, values) { return { amount: values.length }; } ]
+    values = self.map_reduce(map, reduce).out(inline: true)
+    values.select{ |row| row["_id"] }.inject({}){ |h,row| h[row["_id"]]=row["value"]["amount"].to_i; h }
+  end
+
+  def self.page_flow_aggrigation
+    MoveLog.delete_all
+    map = <<-MAP_FUNC
+      function () {
+        emit( this.host, { path: this.path, time: this.time } );
+      }
+    MAP_FUNC
+    reduce = <<-REDUCE_FUNC
+      function (key, values) {
+        values.sort(function(a,b){ return (a.time==b.time) ? 0 : ((a.time>b.time)? 1 : -1); });
+        var cache = values.shift();
+        var moves = [];
+        values.forEach(function(value){
+          moves.push({ from: cache["path"], to: value["path"] });
+          cache = value;
+        });
+        return { moves: moves };
+      }
+    REDUCE_FUNC
+
+    values = self.map_reduce(map, reduce).out(inline: true)
+    values.each do |host|
+      host["value"]["moves"].each do |move|
+        MoveLog.create(move)
+      end
+    end
+  end
 end
