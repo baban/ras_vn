3f05ff48dccdbce44446e8d91fd559b546aaf20b
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-10-23 23:58:36 +0900

    マイページデザイン調整

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 module RecipeHelper
-  def recipe_genre_select_area(f, option)
+  def recipe_genre_select_area( f, option={} )
     tag  = select_tag(:recipe_genre_selecter, options_for_select(RecipeFoodGenre.all.map{ |o| [ o.name,o.id ] }) )
     tag += f.grouped_collection_select(:recipe_food_id, RecipeFoodGenre.all, :recipe_foods, :name, :id, :name, include_blank: true, value: option[:value] )
     tag.html_safe
