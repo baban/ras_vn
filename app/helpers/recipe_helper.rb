ecbac9365702656724f41e7201f988f67cb2158b
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-11-04 20:42:41 +0900

    レシピジャンル選択画面で初期値を自動で入れるようにする

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -3,7 +3,7 @@
 module RecipeHelper
   def recipe_genre_select_area( f, option={} )
     tag  = select_tag(:recipe_genre_selecter, options_for_select(RecipeFoodGenre.all.map{ |o| [ o.name,o.id ] }) )
-    tag += f.grouped_collection_select(:recipe_food_id, RecipeFoodGenre.all, :recipe_foods, :name, :id, :name, include_blank: true, value: option[:value] )
+    tag += f.grouped_collection_select(:recipe_food_id, RecipeFoodGenre.all, :recipe_foods, :name, :id, :name, value: option[:value] )
     tag.html_safe
   end
 end
