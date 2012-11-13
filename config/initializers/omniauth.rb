9260ec08404db95e92554938bff5f41120eab058
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-11-13 23:37:19 +0900

    facebook,twitterログインのIDを挿入

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter,"zardckTK9Vhtw785NlYXpA","HGLKtPNkDxYRfrFeNvDmpfYsTY1VEvQNxqHT5t5kmf4"
-  provider :facebook,"317242041700524","220d33961f61e6375e6e758a73822dc5"
-  provider :google_oauth2, "694847226301.apps.googleusercontent.com", "FyQ-VuzsZR90N7rRKU_x35oM"
+  provider :twitter,       "JaE8SCtn5R5qNITANJPIjw",                   "UDCCYsA0WQPXxLFT2jFPNGVhr4YpwYeML9k3TRk"
+  provider :facebook,      "452090888159529",                          "4e2ddd2159ed37a093054266b0a71d50"
+  provider :google_oauth2, "1046178131442.apps.googleusercontent.com", "jS2F9tJHOo2RcRAiZvH-mkbg"
 end
 
