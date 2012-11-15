8206ccf477568b9ad0799c5c1834ff4743454de6
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-11-16 00:27:22 +0900

    facebookログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,11 @@
 
 class User < ActiveRecord::Base
   establish_connection "cook24_users" if [:staging,:production].include?(Rails.env.to_sym)
-
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :omniuser_id
@@ -30,6 +21,37 @@ class User < ActiveRecord::Base
   has_many :recipe_comments
   has_many :diaries
   has_many :followers
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
+
+  def self.find_for_facebook_oauth( auth, signed_in_resource=nil )
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create( name: auth.extra.raw_info.name,
+                          provider: auth.provider,
+                          uid: auth.uid,
+                          email: auth.info.email,
+                          password: Devise.friendly_token[0,20] )
+    end
+    user
+  end
+
+  def self.find_for_twitter_oauth( auth, signed_in_resource=nil )
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create( name: auth.info.nickname,
+                          provider: auth.provider,
+                          uid: auth.uid,
+                          password: Devise.friendly_token[0,20] )
+    end
+    user
+  end
 
   def initialize(*args)
     super(*args)
--- db/migrate/20120427000030_add_social_fields_to_users.rb
+++ db/migrate/20120427000030_add_social_fields_to_users.rb
@@ -1,10 +1,9 @@
+# encoding: utf-8
+
 class AddSocialFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-
     add_column :users, :uid, :integer
-
     add_column :users, :provider, :string
-
   end
 end
