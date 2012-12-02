63caa0e7334725e7b0df534b27b2f039734fcf02
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-12-02 13:09:08 +0900

    コメント取得

--- app/models/recipe_comment.rb
+++ app/models/recipe_comment.rb
@@ -3,13 +3,17 @@
 class RecipeComment < ActiveRecord::Base
   belongs_to :recipe
 
-  scope :visibles, ->{ where("") }
+  scope :visibles, ->{ scoped }
   scope :topics, ->{ visibles.page(1).per(3) }
 
   mount_uploader :image, RecipeCommentUploader
 
+  def profs
+    UserProfile.where( user_id: self.user_id )
+  end
+
   def prof
-    UserProfile.find_by_user_id(self.user_id)
+    @cache || UserProfile.where( user_id: self.user_id ).first
   end
   alias :chef :prof
 end
--- spec/models/recipe_comments_spec.rb
+++ spec/models/recipe_comments_spec.rb
@@ -1,5 +1,17 @@
+# encoding: utf-8
+
 require 'spec_helper'
 
-describe RecipeComments do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe RecipeComment do
+  fixtures :users, :user_profiles, :user_profile_visibilities, :recipe_comments
+
+  describe "#prof" do
+    before do
+      @comment = RecipeComment.first
+    end
+    it "投稿したユーザーのプロフィールを取得する" do
+      @comment.prof.should be_instance_of UserProfile
+      @comment.prof.user_id.should == 1
+    end
+  end
 end
