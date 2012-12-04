e448715f2cc5c8c06141e0b96d14548fb92345f8
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-12-04 09:59:30 +0900

    編集ボタンが働いていないのを修正

--- app/models/recipe_ranking.rb
+++ app/models/recipe_ranking.rb
@@ -3,11 +3,8 @@
 class RecipeRanking < ActiveRecord::Base
   paginates_per 10
 
-  #scope :topics, ->{ where("").joins(:recipes).on( " recipe_rankings.recipes_id = recipes.id" ) }
-
   def self.topics
-    ids = page(1).per(2).all.map(&:recipe_id)
-    Recipe.where( "id in (?) ", ids )
+    Recipe.where( id: page(1).per(2).pluck(:recipe_id) )
   end
 
   # create ranking
