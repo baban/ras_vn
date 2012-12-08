66cf64be294dc0142470fa7c1beb39f2b7a7f774
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-12-08 11:44:29 +0900

    レシピのジャンル名で検索するオプションを追加

--- app/controllers/recipe_food_genres_controller.rb
+++ app/controllers/recipe_food_genres_controller.rb
@@ -2,10 +2,6 @@
 
 class RecipeFoodGenresController < ApplicationController
   def index
-    if params[:recipe_food_genre_id]
-      @food_genres = RecipeFoodGenre.where( " id = ? ", params[:recipe_food_genre_id] ).includes(:recipe_foods)
-    else
-      @food_genres = RecipeFoodGenre.includes(:recipe_foods)
-    end
+    @food_genres = RecipeFoodGenre.includes(:recipe_foods)
   end
 end
