ccfa688d2713c387c5766c571260dc467a048d85
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-12-15 17:23:14 +0900

    bug:会員登録完了直後で個人情報を入力していないのユーザーをStreamに出していたのを修正　

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -34,13 +34,13 @@ class Devise::RegistrationsController < DeviseController
     #=end
     if resource.save
       if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
+        set_flash_message :notice, "signed_up" if is_navigational_format?
         sign_in(resource_name, resource)
+        #=begin
+        Stream.push( Stream::ADD_USER, resource.id )
+        #=end
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
-        #=begin
-        Stream.push( Stream::ADD_USER, resource.id ) 
-        #=end
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
         #=begin
