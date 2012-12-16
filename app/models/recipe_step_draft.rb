79f6df5944c3d118a48d67725e20cc04b3f2b755
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-12-16 11:39:03 +0900

    fix:レシピのステップでコメントテキストがなくても、画像や動画のみで投稿できるように修正

--- app/models/recipe_step_draft.rb
+++ app/models/recipe_step_draft.rb
@@ -16,7 +16,8 @@ class RecipeStepDraft < ActiveRecord::Base
     last_steps = last_steps[0..form_values.size].push( *[nil]*[(form_values.size-last_steps.size),0].max )
     form_values = [form_values,last_steps].transpose
     steps= form_values.select do |o|
-      o.first[:content].present?
+      draft = o.first
+      draft[:content].present? or draft[:image].present? or draft[:movie_url].present?
     end.map do |params,last_step|
       d = RecipeStepDraft.new(params)
       d.image= File.open(last_step.image.current_path) if d.image.blank? and last_step.try(:image).present? and File.exist?(last_step.image.current_path)
