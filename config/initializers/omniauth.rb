7083a06c873a81c7578a274fe00e0af211c9a173
masanao.matsubara <m_matsubara@okwave.co.jp>
2012-12-26 23:10:22 +0900

    facebookの誕生日取得テスト

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,       "JaE8SCtn5R5qNITANJPIjw",                   "UDCCYsA0WQPXxLFT2jFPNGVhr4YpwYeML9k3TRk"
-  provider :facebook,      "452090888159529",                          "4e2ddd2159ed37a093054266b0a71d50"
+  provider :facebook,      "452090888159529",                          "4e2ddd2159ed37a093054266b0a71d50",       scope: "user_birthday"
   provider :google_oauth2, "1046178131442.apps.googleusercontent.com", "jS2F9tJHOo2RcRAiZvH-mkbg"
 end
 
