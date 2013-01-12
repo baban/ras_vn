d1c6ad90a88479673cd071d62b36578639071a9b
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-12 14:58:53 +0900

    友だちリスト作成機能

--- app/controllers/facebook_friends_controller.rb
+++ app/controllers/facebook_friends_controller.rb
@@ -2,10 +2,12 @@
 
 class FacebookFriendsController < ApplicationController
   def index
-    @friend_list = FbGraph::FriendList.new('100002130858178', access_token: FACEBOOK_ACCESS_TOKEN )
-    @members = @friend_list.members
-    logger.info :members
-    logger.info @members.inspect
+    user = FbGraph::User.fetch('100002130858178', :access_token => FACEBOOK_ACCESS_TOKEN)
+    logger.info :user
+    logger.info user.inspect
+    @friends = user.friends
+    logger.info :friends
+    logger.info @friends.inspect
   end
 
   def invite
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, "1046178131442.apps.googleusercontent.com", "jS2F9tJHOo2RcRAiZvH-mkbg"
 end
 
+# APP_ID = "452090888159529"
+# APP_SECRET = "4e2ddd2159ed37a093054266b0a71d50"
+# app = FbGraph::Application.new(APP_ID, secret: APP_SECRET)
+# app.get_access_token
+
 # access token get metho is written below url
 # https://github.com/nov/fb_graph/wiki/App-Access-Token
 FACEBOOK_ACCESS_TOKEN = "452090888159529|2aLPrLa7PPhxocdT-ECw7SJHkMY"
