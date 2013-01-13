cf0e541729a9cb430c190a8cfd5332b86da9d00c
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-14 02:57:36 +0900

    shutoku

--- app/controllers/facebook_friends_controller.rb
+++ app/controllers/facebook_friends_controller.rb
@@ -2,13 +2,14 @@
 
 class FacebookFriendsController < ApplicationController
   def index
+    # @friends = File.binread(Rails.root.to_path+"/resource/friends.data")
     user = FbGraph::User.fetch('100002130858178', access_token: FACEBOOK_ACCESS_TOKEN)
     logger.info :user
     logger.info user.inspect
     @friends = user.friends
     logger.info :friends
     logger.info @friends.inspect
-    File.binwrite(Rails.root.to_path+"/friends.data", @friends)
+    File.binwrite(Rails.root.to_path+"/resources/friends.data", Marshal.dump(@friends))
   end
 
   def invite
