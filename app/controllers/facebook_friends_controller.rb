d453df59ad07441ca42c56c339e09bffa0aa2e8c
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-14 02:10:36 +0900

    facebook友達リスト取得機能

--- app/controllers/facebook_friends_controller.rb
+++ app/controllers/facebook_friends_controller.rb
@@ -2,12 +2,13 @@
 
 class FacebookFriendsController < ApplicationController
   def index
-    user = FbGraph::User.fetch('100002130858178', :access_token => FACEBOOK_ACCESS_TOKEN)
+    user = FbGraph::User.fetch('100002130858178', access_token: FACEBOOK_ACCESS_TOKEN)
     logger.info :user
     logger.info user.inspect
     @friends = user.friends
     logger.info :friends
     logger.info @friends.inspect
+    File.binwrite(Rails.root.to_path+"/friends.data", @friends)
   end
 
   def invite
