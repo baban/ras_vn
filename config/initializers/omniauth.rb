755c767febf6b34899027af1a6ca3dedf6ab323c
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-16 00:22:15 +0900

    facebook の　notificationを使えるようにする

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter,       "JaE8SCtn5R5qNITANJPIjw",                   "UDCCYsA0WQPXxLFT2jFPNGVhr4YpwYeML9k3TRk"
-  provider :facebook,      "452090888159529",                          "4e2ddd2159ed37a093054266b0a71d50",       scope: "publish_stream,user_birthday,read_friendlists"
+  provider :facebook,      "452090888159529",                          "4e2ddd2159ed37a093054266b0a71d50",       scope: "user_notes,friends_notes,publish_stream,user_birthday,read_friendlists"
   provider :google_oauth2, "1046178131442.apps.googleusercontent.com", "jS2F9tJHOo2RcRAiZvH-mkbg"
 end
 
