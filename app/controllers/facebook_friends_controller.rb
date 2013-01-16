d25c57c2f368ba28859c8d4d5d0538b1f30f96a1
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-17 00:56:27 +0900

    event

--- app/controllers/facebook_friends_controller.rb
+++ app/controllers/facebook_friends_controller.rb
@@ -4,23 +4,9 @@ class FacebookFriendsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    # @friends = Marshal.load(File.binread(Rails.root.to_path+"/resource/friends.dat"))
-    
-    logger.info current_user.inspect
-    omniuser = Omniuser.find(current_user.omniuser_id)
-    logger.info :omniuser
-    logger.info omniuser.inspect
-    return redirect_to "/" unless omniuser.provider=="facebook"
-
-    uid = omniuser.uid
-    logger.info uid
-    user = FbGraph::User.fetch(uid, access_token: FACEBOOK_ACCESS_TOKEN)
-    logger.info :user
-    logger.info user.inspect
-    @friends = user.friends
-    logger.info :friends
-    logger.info @friends.inspect
-    # File.binwrite(Rails.root.to_path+"/friends.data", Marshal.dump(@friends))
+    @headers['Pragma'] = 'public'
+    @headers['Cache-Control'] = "max-age=#{60*60*24*365}"
+    @headers['Expires'] = 1.year.from_now.strftime("%a, %d %b %Y %H:%i:%s")
   end
 
   def invite
