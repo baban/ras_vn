02898c797f3fdd0f58fb59c638a731655ddb28f3
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-19 18:26:15 +0900

    assets高速化

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,6 +11,14 @@ end
 # app = FbGraph::Application.new(APP_ID, secret: APP_SECRET)
 # app.get_access_token
 
+# uid = "100002130858178"
+# friend = FbGraph::User.fetch(uid, access_token: FACEBOOK_ACCESS_TOKEN)
+# friend.notifications
+# friend.notification!( :access_token => FACEBOOK_ACCESS_TOKEN, :href => 'http://matake.jp', :template => 'Your friend achieved new badge!' )
+# app = FbGraph::Application.new("452090888159529", :secret => "4e2ddd2159ed37a093054266b0a71d50")
+# friend = FbGraph::User.fetch(uid)
+# app.notify!( friend,:href => 'http://matake.jp',:template => 'Your friend achieved new badge!')
+
 # access token get metho is written below url
 # https://github.com/nov/fb_graph/wiki/App-Access-Token
 FACEBOOK_ACCESS_TOKEN = "452090888159529|2aLPrLa7PPhxocdT-ECw7SJHkMY"
