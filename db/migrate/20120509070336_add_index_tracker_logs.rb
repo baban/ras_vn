8547db2708fe068c7d5907b851968443a8434771
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-27 21:20:37 +0900

    tracker_index

--- db/migrate/20120509070336_add_index_tracker_logs.rb
+++ db/migrate/20120509070336_add_index_tracker_logs.rb
@@ -3,11 +3,13 @@
 class AddIndexTrackerLogs < ActiveRecord::Migration
   def up
     add_index :tracker_logs, :session_id, unique: true
-    add_index :tracker_logs, :user_id      
+    add_index :tracker_logs, :user_id
+    add_index :tracker_logs, :created_at
   end
 
   def down
     remove_index :tracker_logs, :session_id
-    remove_index :tracker_logs, :user_id      
+    remove_index :tracker_logs, :user_id
+    remove_index :tracker_logs, :created_at
   end
 end
