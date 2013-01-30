2f07ac83d4ec009c29a908f784b619f26009d437
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-01-30 21:25:43 +0900

    おすすめレシピ配信機能

--- app/models/recomment_recipe_mail.rb
+++ app/models/recomment_recipe_mail.rb
@@ -2,5 +2,34 @@
 
 class RecommentRecipeMail < ActiveRecord::Base
   attr_accessible :day, :recipe_id
+
+  def self.send_mail_magazines
+    self.create_mail_buffers
+  end
+
+  # set mail fatas mail buffer table
+  def self.create_mail_buffers
+    Rails.logger.info "mail buffer : create start"
+
+    # get today's mail data
+    mail = MailTemplate.todays_mail
+
+    return false unless mail
+
+    profiles = self.mail_senders_info
+    profiles.find_each( batch_size: 500 ) do |profile|
+      self.create_mail_buffer( profile, mail )
+    end
+
+    Rails.logger.info "mail buffer : create end"
+    true
+  end
+
+  # mail sendable users
+  def self.mail_senders_info
+    users = User.where( retire_flg: false ).select(:id)
+    UserProfile.where( mail_status: 1 ).where( " user_id IN (#{users.to_sql}) " )
+  end
+
 end
 
