55003aaef05d050ef31563c529bea0a9df2470f1
masanao.matsubara <m_matsubara@okwave.co.jp>
2013-02-01 01:48:09 +0900

    メルマガ機能でバッグ

--- app/models/mail_buffer.rb
+++ app/models/mail_buffer.rb
@@ -12,24 +12,6 @@ class MailBuffer < ActiveRecord::Base
     self.send_mail_buffers
   end
 
-  # set mail fatas mail buffer table
-  def self.create_mail_buffers
-    Rails.logger.info "mail buffer : create start"
-
-    # get today's mail data
-    mail = MailTemplate.todays_mail
-
-    return false unless mail
-
-    profiles = self.mail_senders_info
-    profiles.find_each( batch_size: 500 ) do |profile|
-      self.create_mail_buffer( profile, mail )
-    end
-
-    Rails.logger.info "mail buffer : create end"
-    true
-  end
-
   # mail sendable users
   def self.mail_senders_info
     users = User.where( retire_flg: false ).select(:id)
@@ -58,6 +40,7 @@ class MailBuffer < ActiveRecord::Base
       next unless mail
       begin
         mail.deliver
+        mail.delete
       rescue => e
         Rails.logger.error " mail send error : #{mail.inspect} "
         FailMail.set_fail_mail( buffer.to )
